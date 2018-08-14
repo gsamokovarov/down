ee3543a6600a6eba7dc3501c5074f7d84633d6ac
Genadi Samokovarov <gsamokovarov@gmail.com>
2018-08-14 10:26:49 +0300

    Touch upon the documentation

--- down.go
+++ down.go
@@ -10,20 +10,31 @@ type Downloader interface {
 	Download(string) (Blob, error)
 }
 
-// Blob is a downloaded file. Either stored on a regular filesystem, remotely
-// or in-memory.
+// Blob is a downloaded file. It can be physically stored on disk or streamed
+// remotely as being read.
 type Blob interface {
 	io.ReadCloser
+
+	// Location is the location of the file. Can be an absolute path, can be a
+	// remote URL or can be empty if it's not representable.
 	Location() string
+
+	// Release frees up the file. If it's stored on disk, this will mean removing
+	// the file from it.
 	Release() error
 }
 
-// NewFileDownloader downloads an URL to a temporary file.
-func NewFileDownloader(dir string, option ...*http.Client) Downloader {
-	return &fileDownloader{dir, getOptionalHTTPClient(option...)}
+// NewFileDownloader downloads an URL to a file. A directory has to be
+// specified for the file to be stored in. Can be used for temporary file
+// downloads if dir is os.TempDir.
+func NewFileDownloader(dir string, client ...*http.Client) Downloader {
+	return &fileDownloader{dir, getOptionalHTTPClient(client...)}
 }
 
-// NewPipeDownloader downloads an URL on demand.
-func NewPipeDownloader(option ...*http.Client) Downloader {
-	return &pipeDownloader{getOptionalHTTPClient(option...)}
+// NewPipeDownloader downloads an URL on demand. This downloader won't save the
+// file on disk, but will stream it directly as the blob is read. Can be used
+// to download a remote file from one location and upload it to another remote
+// one.
+func NewPipeDownloader(client ...*http.Client) Downloader {
+	return &pipeDownloader{getOptionalHTTPClient(client...)}
 }
